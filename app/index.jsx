78be70718d33dba5e022a6202d3f5ab8e515610a
AugustoLeoni <jaleonimartins@hotmail.com>
2025-10-01 22:16:28 -0300

    Adicionado estilos com StyleSheet e adicionado imagens

---
+++ app/index.jsx
@@ -0,0 +1,75 @@
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Image resizeMode="contain" style={styles.image} source={require('./Imagem foco.png')}/>
+      <View style={styles.action}>
+        <Text style={styles.timer}>
+          25:00
+        </Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>
+            Começar
+          </Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais.
+        </Text>
+        <Text style={styles.footerText}>
+          Desenvolvido por Alura. 
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#021123",
+    gap: 40
+  },
+  image: {
+    width: 317.67,
+  },
+  action: {
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: "#14448080",
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32
+  },
+  timer: {
+    color: "#FFF",
+    fontSize: 54,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: "#B872FF",
+    borderRadius: 32,
+    padding: 8,
+  },
+  buttonText: {
+    color: "#021123",
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: "center"
+  },
+  footer: {
+    width: '80%',
+  },
+  footerText: {
+    textAlign: 'center',
+    color: '#98A0A8',
+    fontSize: 12.5
+  }
+})
